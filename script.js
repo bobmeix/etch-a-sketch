42e25f8b2788fc052c289051d1e9c4eb1e269752
bobmeix <bobmeix@yahoo.com>
2022-08-09 01:05:59 +0300

    Decrease square size as a test

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const container = document.querySelector('.container');
 
-for (let i = 1; i <= 256; i++) {
+for (let i = 1; i <= 25600; i++) {
     const div = document.createElement('div');
     div.classList.add('square-div');
-    div.textContent = i;
+    //div.textContent = i;
     container.appendChild(div);
     div.addEventListener('mouseover', changeBackgroundColor);
 }

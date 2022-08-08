ea3e1e661b3766b74e3328a3e84d730ca39972e4
bobmeix <bobmeix@yahoo.com>
2022-08-09 02:36:42 +0300

    Add delete functionality using right click

--- script.js
+++ script.js
@@ -5,10 +5,13 @@ for (let i = 1; i <= 1024; i++) {
     div.classList.add('square-div');
     container.appendChild(div);
     div.addEventListener('mouseover', changeBackgroundColor);
+    div.addEventListener('contextmenu', e => e.preventDefault());
 }
 
 function changeBackgroundColor(e) {
     if (e.buttons === 1) {
         e.target.classList.add('square-div-changed');
+    } else if (e.buttons === 2) {
+        e.target.classList.remove('square-div-changed');
     }
 }

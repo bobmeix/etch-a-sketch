c3748ec7ed63e29d7d01ce58c3f980044cca9670
bobmeix <bobmeix@yahoo.com>
2022-08-09 22:52:18 +0300

    Add rainbow color selection
    It's finally working!

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ const border = document.querySelector('.border');
 const container = document.querySelector('.container');
 const clearButton = document.querySelector('#clear');
 clearButton.addEventListener('click', clearContent);
+
+// let blackButton = false;
+// let grayButton = true;
+let rainbowButton = false;
+
 const colorButtons = document.querySelectorAll('.color-button');
 colorButtons.forEach(button => {
     button.addEventListener('click', selectDrawColor);
 })
-// const blackButton = document.querySelector('#black');
-// const grayButton = document.querySelector('#gray');
+
+// console.log(colorButtons[2].id);
+
 let drawColor = '#aca2a0';
 let backgroundColor = '#fcf2f0';
 let black = '#000';
-let gray = '#808080';
-
-// blackButton.addEventListener('click', selectDrawColor);
-// grayButton.addEventListener('click', select)
+let brick = '#C13E22';
 
 function initBoard() {
     for (let i = 1; i <= 1024; i++) {
@@ -27,41 +30,64 @@ function initBoard() {
 }
 
 function changeColor(e) {
-    if (e.buttons === 1) {
-        e.target.setAttribute('style', `background: ${drawColor};`);
+    if (e.buttons === 1 && rainbowButton) {
+        console.log(rainbowButton);
+        e.target.setAttribute('style', `background: ${generateRandomColors()};`);
+    } else if (e.buttons === 1) {
+                e.target.setAttribute('style', `background: ${drawColor};`);
     } else if (e.buttons === 2) {
-        e.target.setAttribute('style', `background: ${backgroundColor};`);
+        e.target.setAttribute('style', `background: #fff;`);
     }
 }
 
 function clearContent() {
     let square = document.querySelectorAll('.square-div');
     square.forEach(square => {
-        square.setAttribute('style', `background: ${backgroundColor};`);
+        square.setAttribute('style', `background: #fff;`);
     });
 }
 
 function selectDrawColor(color) {
     if (color.target.id === 'black') {
         drawColor = black;
+        rainbowButton = false;
         colorButtons.forEach(button => {
             if (button.id === 'black') {
-                button.setAttribute('style', `background: ${gray}`);
+                button.setAttribute('style', `color: ${backgroundColor}; background: ${brick}`);
             } else {
-                button.setAttribute('style', `background: ${backgroundColor}`);
+                button.setAttribute('style', `color: black; background: ${backgroundColor}`);
             }
         });
+        return drawColor;
     } else if (color.target.id === 'gray') {
         drawColor = '#aca2a0';
+        rainbowButton = false;
         colorButtons.forEach(button => {
             if (button.id === 'gray') {
-                button.setAttribute('style', `background: ${gray}`);
+                button.setAttribute('style', `color: ${backgroundColor}; background: ${brick}`);
             } else {
-                button.setAttribute('style', `background: ${backgroundColor}`);
+                button.setAttribute('style', `color: black; background: ${backgroundColor}`);
+            }
+        });
+        return drawColor;
+    } else if (color.target.id === 'rainbow') {
+        rainbowButton = true;
+        colorButtons.forEach(button => {
+            if (button.id === 'rainbow') {
+                button.setAttribute('style', `color: ${backgroundColor}; background: ${brick}`);
+            } else {
+                button.setAttribute('style', `color: black; background: ${backgroundColor}`);
             }
         });
     }
-    return drawColor;
+}
+
+function randomColor() {
+    return Math.floor(Math.random() * 256);
+}
+
+function generateRandomColors() {
+    return `rgb(${randomColor()}, ${randomColor()}, ${randomColor()})`;
 }
 
 initBoard();

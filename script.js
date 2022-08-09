dec6b2e29e5fbd3a8cd58b4e37b08e87526c2e3a
bobmeix <bobmeix@yahoo.com>
2022-08-09 14:56:40 +0300

    Add buttons
    - clear button functional
    - replaced classList.add with setAttribute

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+const border = document.querySelector('.border');
 const container = document.querySelector('.container');
+const clearButton = document.querySelector('#clear');
+const blackButton = document.querySelector('#black');
+let drawColor = '#aca2a0';
+let backgroundColor = '#fcf2f0';
 
-for (let i = 1; i <= 1024; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square-div');
-    container.appendChild(div);
-    div.addEventListener('mouseover', changeBackgroundColor);
-    div.addEventListener('contextmenu', e => e.preventDefault());
-}
+clearButton.addEventListener('click', clearContent);
 
-function changeBackgroundColor(e) {
-    if (e.buttons === 1) {
-        e.target.classList.add('square-div-changed');
-    } else if (e.buttons === 2) {
-        e.target.classList.remove('square-div-changed');
+function initBoard() {
+    for (let i = 1; i <= 1024; i++) {
+        let square = document.createElement('div');
+        square.classList.add('square-div');
+        container.appendChild(square);
+        square.addEventListener('mouseover', changeColor);
+        square.addEventListener('contextmenu', e => e.preventDefault());
     }
 }
+
+function changeColor(e) {
+    if (e.buttons === 1) {
+        e.target.setAttribute('style', `background: ${drawColor};`);
+    } else if (e.buttons === 2) {
+        e.target.setAttribute('style', `background: ${backgroundColor};`);
+    }
+}
+
+function clearContent() {
+    let square = document.querySelectorAll('.square-div');
+    square.forEach(square => {
+        square.setAttribute('style', `background: ${backgroundColor};`);
+    });
+}
+
+initBoard();
